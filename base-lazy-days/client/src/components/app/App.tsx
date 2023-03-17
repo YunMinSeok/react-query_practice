3d8d2ef790461ca5d6a5b63795c143150333678f
윤민석 <ykmvm147@naver.com>
2023-03-17 21:08:12 +0900

    feat: react-query 설치 완료

--- base-lazy-days/client/src/components/app/App.tsx
+++ base-lazy-days/client/src/components/app/App.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { ReactElement } from 'react';
 
+import { queryClient } from '../../react-query/queryClient';
 import { theme } from '../../theme';
 import { Loading } from './Loading';
 import { Navbar } from './Navbar';
