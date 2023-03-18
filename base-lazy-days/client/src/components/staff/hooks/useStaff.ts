e3dea5e74e246b068378c7616da56acdab61dcbc
윤민석 <ykmvm147@naver.com>
2023-03-18 20:31:20 +0900

    feat: code quiz staff 쪽 구현 완ㄹ

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -3,13 +3,14 @@ import { Dispatch, SetStateAction, useState } from 'react';
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
+import { useQuery } from 'react-query';
 import { filterByTreatment } from '../utils';
 
 // for when we need a query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
 
 interface UseStaff {
   staff: Staff[];
@@ -22,7 +23,8 @@ export function useStaff(): UseStaff {
   const [filter, setFilter] = useState('all');
 
   // TODO: get data from server via useQuery
-  const staff = [];
+  const fallback = [];
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff);
 
   return { staff, filter, setFilter };
 }
